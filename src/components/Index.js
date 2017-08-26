536ed7fc6cef546a02bc7cf24907770819e8116b
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2017-08-26 16:31:41 -0300

    feat: Add editor config

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => (
-    <h1>Hello React-Redux</h1>
+  <h1>Hello React-Redux</h1>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
