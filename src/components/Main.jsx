5432372803cb4b298fabc2374ce12ff7b46b7dfc
Gabriel Anzaldo <gabo77zurdex@gmail.com>
2017-09-03 12:25:07 -0300

    chore: Add http service, create a wrapper over fetch to call server methods

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import uuidv4 from 'uuid/v4';
+
 import * as BlogAPI from '../http-service';
 
 class Main extends Component {
   componentDidMount() {
-    BlogAPI.getAllCategories().then(data => console.log(data.categories));
+    // post example
+    const temPost = {
+      id: uuidv4(),
+      timestamp: Date.now(),
+      title: 'gabriel y deysi',
+      body: 'an awesome body',
+      owner: 'gabriel y deysi',
+      category: 'redux',
+    };
+    BlogAPI.getPost('31b0c141-0ced-4157-83c3-657272f31cc7')
+      .then(data => console.log(data, temPost));
   }
 
   render() {
--- src/http-service.js
+++ src/http-service.js
@@ -1,8 +1,30 @@
-export const getAllCategories = () => fetch('http://localhost:5001/categories',
-  { headers: { Authorization: 'granted' } })
+const api = 'http://localhost:5001';
+const defaultParameters = { headers: { Authorization: 'granted' } };
+
+export const getAllCategories = () => fetch(`${api}/categories`,
+  defaultParameters)
   .then(response => response.json());
 
-export const getAllCategories1 = () => fetch('http://localhost:5001/categories',
-  { headers: { Authorization: 'granted' } })
+export const getCategoryPosts = category => fetch(`${api}/${category}/posts`,
+  defaultParameters)
   .then(response => response.json());
 
+export const getAllPosts = () => fetch(`${api}/posts`,
+  defaultParameters)
+  .then(response => response.json());
+
+export const addNewPost = postData => fetch(`${api}/posts`,
+  {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'granted',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postData),
+  })
+  .then(response => response.json());
+
+export const getPost = postId => fetch(`${api}/posts/${postId}`,
+  defaultParameters)
+  .then(response => response.json());
