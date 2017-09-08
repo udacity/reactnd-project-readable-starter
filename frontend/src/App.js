2d9c81df2b729376151edb80377a45d9762e189e
Richard Kalehoff <richardkalehoff@gmail.com>
2017-09-08 15:04:36 -0700

    Allow project to communicate with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backend: 'backend-data'
+    }
+  }
+
+  componentDidMount() {
+    const url = `${process.env.REACT_APP_BACKEND}/categories`;
+    console.log('fetching from url', url);
+    fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
+                 credentials: 'include' } )
+      .then( (res) => { return(res.text()) })
+      .then((data) => {
+        this.setState({backend:data});
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +30,10 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Talking to the backend yields these categories: <br/>
+          {this.state.backend}
         </p>
       </div>
     );
