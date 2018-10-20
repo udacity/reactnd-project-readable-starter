42829220ddbfdf5ba3af7878c37ff139ba1be76d
Roni <roni.c.castro@gmail.com>
2018-10-19 21:56:19 -0300

    Update menu style

--- frontend/src/components/NavDropdownMenu.js
+++ frontend/src/components/NavDropdownMenu.js
@@ -1,33 +1,46 @@
-
 import React from 'react';
-import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
 
 class NavDropdownMenu extends React.Component {
-    handleSelect(eventKey, event) {
-      event.preventDefault();
-      alert(`selected ${eventKey}`);
-    }
-  
+ constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
     render() {
       return (
-        <Nav bsStyle="tabs" activeKey="1" onSelect={k => this.handleSelect(k)}>
-          <NavItem eventKey="1" href="/home">
-            NavItem 1 content
-          </NavItem>
-          <NavItem eventKey="2" title="Item">
-            NavItem 2 content
-          </NavItem>
-          <NavItem eventKey="3" disabled>
-            NavItem 3 content
-          </NavItem>
-          <NavDropdown eventKey="4" title="Dropdown" id="nav-dropdown">
-            <MenuItem eventKey="4.1">Action</MenuItem>
-            <MenuItem eventKey="4.2">Another action</MenuItem>
-            <MenuItem eventKey="4.3">Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey="4.4">Separated link</MenuItem>
-          </NavDropdown>
-        </Nav>
+        <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Read</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
       );
     }
   }
