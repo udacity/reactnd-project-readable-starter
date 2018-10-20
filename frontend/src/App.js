d298bd6c9910a8a509eac2eedab5367619d1a7ff
Roni <roni.c.castro@gmail.com>
2018-10-19 21:32:35 -0300

    Create NavDropdownMenu component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavDropdownMenu from './components/NavDropdownMenu'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <NavDropdownMenu />
       </div>
     );
   }
